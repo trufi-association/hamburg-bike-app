69328dcfccc8e1403830942f36d0281cd93458b8
Gustavo TIto <42806689+GustavoTCh@users.noreply.github.com>
2021-09-27 19:57:20 -0400

    [Feature] setting socialMedia

--- lib/drawer_menu/drawer_menu.dart
+++ lib/drawer_menu/drawer_menu.dart
@@ -6,9 +6,8 @@ import 'package:trufi_core/models/menu/menu_item.dart';
 import 'package:flutter/material.dart';
 import 'package:trufi_core/models/menu/social_media/social_media_item.dart';
 import 'package:trufi_core/models/menu/social_media/facebook_social_media.dart';
-import 'package:trufi_core/models/menu/social_media/instagram_social_media.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:trufi_core/models/menu/social_media/twitter_social_media.dart';
-import 'package:trufi_core/models/menu/social_media/website_social_media.dart';
 import 'package:trufi_core/pages/feedback.dart';
 import 'package:trufi_core/pages/home/home_page.dart';
 import 'package:trufi_core/pages/saved_places/saved_places.dart';
@@ -116,6 +115,42 @@ class ImprintButtonMenu extends MenuItem {
         );
 }
 
+class YoutubeSocialMedia extends SocialMediaItem {
+  YoutubeSocialMedia(String url)
+      : super(
+          url: url,
+          buildIcon: (context) => SvgPicture.string(
+            '<svg height="682pt" viewBox="-21 -117 682.66672 682" width="682pt" xmlns="http://www.w3.org/2000/svg"><path d="m626.8125 64.035156c-7.375-27.417968-28.992188-49.03125-56.40625-56.414062-50.082031-13.703125-250.414062-13.703125-250.414062-13.703125s-200.324219 0-250.40625 13.183593c-26.886719 7.375-49.03125 29.519532-56.40625 56.933594-13.179688 50.078125-13.179688 153.933594-13.179688 153.933594s0 104.378906 13.179688 153.933594c7.382812 27.414062 28.992187 49.027344 56.410156 56.410156 50.605468 13.707031 250.410156 13.707031 250.410156 13.707031s200.324219 0 250.40625-13.183593c27.417969-7.378907 49.03125-28.992188 56.414062-56.40625 13.175782-50.082032 13.175782-153.933594 13.175782-153.933594s.527344-104.382813-13.183594-154.460938zm-370.601562 249.878906v-191.890624l166.585937 95.945312zm0 0"/></svg>',
+            height: 25,
+            width: 25,
+            color: Colors.grey,
+          ),
+          name: (BuildContext context) {
+            return Localizations.localeOf(context).languageCode == 'en'
+                ? "Follow us on YouTube"
+                : "Folge uns auf YouTube";
+          },
+        );
+}
+
+class LinkdInSocialMedia extends SocialMediaItem {
+  LinkdInSocialMedia(String url)
+      : super(
+          url: url,
+          buildIcon: (context) => SvgPicture.string(
+            '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>',
+            height: 25,
+            width: 25,
+            color: Colors.grey,
+          ),
+          name: (BuildContext context) {
+            return Localizations.localeOf(context).languageCode == 'en'
+                ? "Follow us on LinkdIn"
+                : "Folge uns auf LinkdIn";
+          },
+        );
+}
+
 final List<List<MenuItem>> menuItems = [
   [
     MenuPageItem(
@@ -189,11 +224,13 @@ final List<List<MenuItem>> menuItems = [
               value.onClick(context, true);
             },
             items: <SocialMediaItem>[
-              // TODO use hamburgo bike app links
-              FacebookSocialMedia("https://www.facebook.com/trufiapp"),
-              InstagramSocialMedia("https://www.instagram.com/trufi.app"),
+              YoutubeSocialMedia(
+                "https://www.youtube.com/channel/UCL_K2RPU0pxV5VYw0Aj_PUA",
+              ),
+              FacebookSocialMedia("https://www.facebook.com/TrufiAssoc"),
               TwitterSocialMedia("https://twitter.com/TrufiAssoc"),
-              WebSiteSocialMedia("https://www.trufi.app/blog/"),
+              LinkdInSocialMedia(
+                  "https://www.linkedin.com/company/trufi-association/"),
             ].map((SocialMediaItem value) {
               return DropdownMenuItem<SocialMediaItem>(
                 value: value,
