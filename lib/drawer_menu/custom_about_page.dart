7acaa95da4163907578fabfaca691219dea06657
Gustavo TIto <42806689+GustavoTCh@users.noreply.github.com>
2021-09-27 19:36:49 -0400

    [Feature] create accordion for the 'about view'

--- lib/drawer_menu/custom_about_page.dart
+++ lib/drawer_menu/custom_about_page.dart
@@ -16,7 +16,10 @@ class _CustomAboutPageState extends State<CustomAboutPage> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme =
-        theme.textTheme.subtitle1.copyWith(fontSize: 13, height: 1.5);
+        theme.textTheme.subtitle1.copyWith(fontSize: 16, height: 1.5);
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+    final isEnglish = Localizations.localeOf(context).languageCode == "en";
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: const Color(0xFFF4F4F4),
@@ -72,91 +75,158 @@ class _CustomAboutPageState extends State<CustomAboutPage> {
               ),
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: ListView(
-              children: [
-                const SizedBox(height: 40),
-                Text(
-                  Localizations.localeOf(context).languageCode == "en"
-                      ? "About Us"
-                      : "Über Uns",
-                  style: theme.textTheme.bodyText2.copyWith(
-                    fontSize: 34,
-                    fontWeight: FontWeight.w600,
-                    fontStyle: FontStyle.italic,
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: ListView(
+                children: [
+                  const SizedBox(height: 40),
+                  Text(
+                    isEnglish ? "About Us" : "Über Uns",
+                    style: theme.textTheme.bodyText2.copyWith(
+                      fontSize: 34,
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.italic,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 24),
-                Text(
-                  Localizations.localeOf(context).languageCode == "en"
-                      ? "We are the coolest group ever! Any other questions?"
-                      : "Wir sind eine echt coole Truppe. Noch Fragen?",
-                  style: theme.textTheme.subtitle1.copyWith(
-                    fontSize: 14,
+                  const SizedBox(height: 24),
+                  Theme(
+                    data: Theme.of(context)
+                        .copyWith(dividerColor: Colors.transparent),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 4,
+                            offset: const Offset(1, 2),
+                          ),
+                        ],
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(3),
+                        child: ExpansionTile(
+                          title: Text(
+                            isEnglish
+                                ? "We are the coolest group ever! Any other questions?"
+                                : "Wir sind eine echt coole Truppe. Noch Fragen?",
+                            style: textTheme,
+                          ),
+                          children: [
+                            RichText(
+                              text:
+                                  TextSpan(style: textTheme, children: <TextSpan>[
+                                TextSpan(
+                                  text: isEnglish
+                                      ? "Dear Bike and Public Transport User!\n\n"
+                                      : "Lieber Fahrrad- und ÖPNV-Nutzer!\n\n",
+                                ),
+                                TextSpan(
+                                  text: "Nicht ohne mein Rad",
+                                  style: textTheme.copyWith(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(
+                                  text: isEnglish
+                                      ? " unites bicycle and public transport"
+                                          " journeys in Hamburg, making life easier for people"
+                                          " who use bikes as their main means of transport. "
+                                          " Defy long trips and rainy days by using public "
+                                          " transport without abandoning your bike! ITS World Congress "
+                                          " and the Hamburg Transport Association (HVV) chose "
+                                          " Trufi Association to create a first-of-its-kind bike app, "
+                                          " and voilà, here it is: "
+                                      : " vereint Fahrrad und ÖPNV in "
+                                          "Hamburg und macht das Leben für Menschen, "
+                                          "die das Fahrrad als Hauptverkehrsmittel nutzen, "
+                                          "einfacher. In Zukunft kannst du langen Wegen oder "
+                                          "Regentagen mit der Nutzung der ÖPNV die Stirn "
+                                          "bieten ohne auf dein Rad zu verzichten. Deshalb "
+                                          "haben wir von Trufi Association im Auftrag der ITS "
+                                          "und der HVV Nägel mit Köpfen gemacht und voilà: "
+                                          "Hier ist sie die ",
+                                ),
+                                TextSpan(
+                                  text: isEnglish
+                                      ? "Nicht ohne mein Rad."
+                                      : "„Nicht ohne mein Rad App“",
+                                  style: textTheme.copyWith(
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                TextSpan(
+                                  text: isEnglish
+                                      ? "!\n\nTrufi Association is an international NGO that promotes"
+                                          "easier access to public transport. Official maps, apps,"
+                                          "and schedules don’t provide all the routes, or they"
+                                          "simply don’t exist in many cities. We fill in the"
+                                          "gaps – and sometimes even map the routes from scratch."
+                                          "Our apps help everyone find the best way to get from "
+                                          "Point A to Point B within their cities. Well-designed "
+                                          "mobility contributes to greater sustainability, cleaner"
+                                          "air and better quality of life.\n"
+                                      : "\n\nTrufi Association ist eine internationale NGO, "
+                                          "die sich für einen leichteren Zugang zu öffentlichen "
+                                          "Verkehrsmitteln einsetzt. Offizielle Karten, Apps "
+                                          "und Fahrpläne enthalten oft nicht alle Routen. In "
+                                          "vielen Städten dieser Welt gibt es sogar gar keine "
+                                          "Fahrpläne. Wir füllen Lücken oder zeichnen Routen "
+                                          "von Grund auf neu auf. Unsere Apps helfen jedem, den "
+                                          "besten Weg zu finden, um in seiner Stadt von A nach B "
+                                          "zu kommen. Gut durchdachte Mobilität trägt zu mehr "
+                                          "Nachhaltigkeit, sauberer Luft und mehr Lebensqualität bei.\n",
+                                ),
+                              ]),
+                            ),
+                          ],
+                          backgroundColor: Colors.white,
+                          collapsedBackgroundColor: Colors.white,
+                          collapsedIconColor: theme.textTheme.subtitle1.color,
+                          iconColor: theme.textTheme.subtitle1.color,
+                          tilePadding:
+                              EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+                          childrenPadding:
+                              EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 24),
-                Text(
-                  Localizations.localeOf(context).languageCode == "en"
-                      ? "Dear Bike and Public Transport User!\n\n"
-                          "Nicht ohne mein Rad unites bicycle and public transport"
-                          " journeys in Hamburg, making life easier for people"
-                          " who use bikes as their main means of transport. "
-                          " Defy long trips and rainy days by using public "
-                          " transport without abandoning your bike! ITS World Congress "
-                          " and the Hamburg Transport Association (HVV) chose "
-                          " Trufi Association to create a first-of-its-kind bike app, "
-                          " and voilà, here it is: Nicht ohne mein Rad!\n\n"
-                          "Trufi Association is an international NGO that promotes"
-                          "easier access to public transport. Official maps, apps,"
-                          "and schedules don’t provide all the routes, or they"
-                          "simply don’t exist in many cities. We fill in the"
-                          "gaps – and sometimes even map the routes from scratch."
-                          "Our apps help everyone find the best way to get from "
-                          "Point A to Point B within their cities. Well-designed "
-                          "mobility contributes to greater sustainability, cleaner"
-                          "air and better quality of life."
-                      : "Lieber Fahrrad- und ÖPNV-Nutzer!\n\n"
-                          "Nicht ohne mein Rad vereint Fahrrad und ÖPNV in "
-                          "Hamburg und macht das Leben für Menschen, "
-                          "die das Fahrrad als Hauptverkehrsmittel nutzen, "
-                          "einfacher. In Zukunft kannst du langen Wegen oder "
-                          "Regentagen mit der Nutzung der ÖPNV die Stirn "
-                          "bieten ohne auf dein Rad zu verzichten. Deshalb "
-                          "haben wir von Trufi Association im Auftrag der ITS "
-                          "und der HVV Nägel mit Köpfen gemacht und voilà: "
-                          "Hier ist sie die Nicht ohne mein Rad!\n\n"
-                          "Trufi Association ist eine internationale NGO, "
-                          "die sich für einen leichteren Zugang zu öffentlichen "
-                          "Verkehrsmitteln einsetzt. Offizielle Karten, Apps "
-                          "und Fahrpläne enthalten oft nicht alle Routen. In "
-                          "vielen Städten dieser Welt gibt es sogar gar keine "
-                          "Fahrpläne. Wir füllen Lücken oder zeichnen Routen "
-                          "von Grund auf neu auf. Unsere Apps helfen jedem, den "
-                          "besten Weg zu finden, um in seiner Stadt von A nach B "
-                          "zu kommen. Gut durchdachte Mobilität trägt zu mehr "
-                          "Nachhaltigkeit, sauberer Luft und mehr Lebensqualität bei.",
-                  style: textTheme,
-                ),
-                const SizedBox(height: 24),
-                Center(
-                  child: Image.asset(
-                    'assets/custom_images/about-us.png',
-                    fit: BoxFit.fill,
-                    width: 250,
+                  const SizedBox(height: 60),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          child: Image.asset(
+                            'assets/custom_images/about-us.png',
+                            fit: BoxFit.fill,
+                            width: isPortrait ? null : 200,
+                          ),
+                        ),
+                        SizedBox(width: isPortrait ? 20 : 60),
+                        Flexible(
+                          child: Image.asset(
+                            'assets/custom_images/logo-hvv.png',
+                            fit: BoxFit.fill,
+                            width: isPortrait ? null : 200,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(height: 50),
-                Center(
-                  child: Image.asset(
-                    'assets/custom_images/trufi-logo.png',
-                    fit: BoxFit.fill,
-                    width: 175,
+                  const SizedBox(height: 50),
+                  Center(
+                    child: Image.asset(
+                      'assets/custom_images/trufi-logo.png',
+                      fit: BoxFit.fill,
+                      width: 115,
+                    ),
                   ),
-                ),
-                const SizedBox(height: 50),
-              ],
+                  const SizedBox(height: 50),
+                ],
+              ),
             ),
           ),
         ],
